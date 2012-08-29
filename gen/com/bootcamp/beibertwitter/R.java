c130d62fba4acc3662ece0810cdae2c80d51be82
Nik Illerbrun <nik.illerbrun@gmail.com>
2012-08-29 01:59:13 -0400

    updated layouts

--- gen/com/bootcamp/beibertwitter/R.java
+++ gen/com/bootcamp/beibertwitter/R.java
@@ -15,13 +15,13 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int filterButton=0x7f050000;
-        public static final int filterWait=0x7f050001;
-        public static final int tweetFeed=0x7f050002;
-        public static final int tweet_image=0x7f050004;
-        public static final int tweet_poster=0x7f050003;
-        public static final int tweet_text=0x7f050005;
-        public static final int tweet_time=0x7f050006;
+        public static final int filterButton=0x7f060000;
+        public static final int filterWait=0x7f060001;
+        public static final int tweetFeed=0x7f060002;
+        public static final int tweet_image=0x7f060004;
+        public static final int tweet_poster=0x7f060003;
+        public static final int tweet_text=0x7f060005;
+        public static final int tweet_time=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -31,5 +31,10 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int displayPic=0x7f040002;
         public static final int hello=0x7f040000;
+    }
+    public static final class style {
+        public static final int footerText=0x7f050002;
+        public static final int headerText=0x7f050000;
+        public static final int mainText=0x7f050001;
     }
 }
